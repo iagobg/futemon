769c6772873491a0db7b6968d1147f713439b765
IagoBG <iago.bussoletti@gmail.com>
2026-07-01 17:30:42 -0300

    tailwind compilation for prod

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./internal/app/templates/*.html",
+    "./internal/app/**/*.go",
+    "./internal/app/static/*.js"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
